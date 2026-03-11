87616924ffada180b866af3abfe586143719d29e
Drew Cain <groksrc@gmail.com>
2026-03-10 22:08:00 -0500

    chore: update version to 0.20.0 for v0.20.0 release

--- src/basic_memory/__init__.py
+++ src/basic_memory/__init__.py
@@ -1,7 +1,7 @@
 """basic-memory - Local-first knowledge management combining Zettelkasten with knowledge graphs"""
 
 # Package version - updated by release automation
-__version__ = "0.19.2"
+__version__ = "0.20.0"
 
 # API version for FastAPI - independent of package version
 __api_version__ = "v0"
